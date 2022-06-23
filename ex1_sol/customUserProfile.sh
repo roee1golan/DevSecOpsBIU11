34853a22f3228accf9a93c549445bbddc5d7c9fd
refael sachevski <refael329012gmail.com>
2022-06-23 22:53:50 +0300

    ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,9 +1,9 @@
 echo Hello $USER
 /usr/lib/update-notifier/apt-check --human-readable
 
-cd ~
+
 file=".token"
-if [[ '-f $file' && `stat -c %a $file` != 600 ]] ; then
+if [[ -f $file && `stat -c %a $file` != 600 ]] ; then
     echo 'Warning: .token file has too open permissions'
 fi
 
