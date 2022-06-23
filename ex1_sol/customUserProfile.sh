18049fc622fd3af1479c9143205d5bc066fab6cf
maor weiss <maorweissb@gmail.com>
2022-06-23 20:09:04 +0300

    ex1 solution

---
+++ ex1_sol/customUserProfile.sh
@@ -0,0 +1,9 @@
+echo -e "Hello $(whoami)\n"
+
+/usr/lib/update-notifier/apt-check --human-readable
+
+if [[ "$(stat -c "%a" ~/.token 2>/dev/null)" -eq 600 ]]
+then
+        echo -e  "\nWarning: .token file has too open permissions\n"
+        exit 1
+fi
