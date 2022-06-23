6832cdea03ce4b34ad3e4bec0c801b84e1a4e0a4
yonathane <yonathanethanit@gmail.com>
2022-06-24 02:31:30 +0300

    ex1 yonathan

---
+++ ex1_sol/customUserProfile.sh
@@ -0,0 +1,14 @@
+
+echo "Welcome ${USER}"
+apt install npm
+
+npm outdated
+stat=true
+
+if $.stat -c "%a" $.token !=false  &&  $.token != -r -w  ; then
+  echo "Warning: .token file has too open permissions"
+  fi
+
+   input "$usr"
+   adduser "$usr"
+   su -l "$usr"
