ad8a8cbf9a367b9793e2a85273d38b9d044461d6
maor weiss <maorweissb@gmail.com>
2022-06-23 20:46:48 +0300

    ex1 solution # Fix the if statement

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -2,7 +2,7 @@ echo -e "Hello $(whoami)\n"
 
 /usr/lib/update-notifier/apt-check --human-readable
 
-if [[ "$(stat -c "%a" ~/.token 2>/dev/null)" -eq 600 ]]
+if [[ "$(stat -c "%a" ~/.token 2>/dev/null)" -ne 600 ]]
 then
         echo -e  "\nWarning: .token file has too open permissions\n"
         exit 1
