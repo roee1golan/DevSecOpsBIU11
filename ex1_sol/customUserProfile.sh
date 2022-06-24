33e4ce0251bd086ee619c7c7ebae3b435b43ce31
Ogen <ogencohen98@gmail.com>
2022-06-24 14:30:40 +0300

    ex solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+TOKEN_F=.token
+TOKEN_PATH=~/$TOKEN_F
+PREM_CHECK=600
+echo Hello $USER
+/usr/lib/update-notifier/apt-check --human-readable
+
+if [ -f  $TOKEN_PATH ];then
+        if stat -c "%a" $TOKEN_PATH | grep -q -v $PREM_CHECK;then
+                        echo Warning : $TOKEN_F file has too open permissions
+        fi
+else
+echo file $TOKEN_F dose not exsit in $TOKEN_PATH "!!"
+fi
