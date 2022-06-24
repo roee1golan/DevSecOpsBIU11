ab37093c9f5cb4423763f0aa47196dac152237e5
hohifrog <hohironyanai@gmail.com>
2022-06-24 18:13:38 +0300

    ex1_solution (i hope)

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
+#print welcome msg
+echo Hello Dear $USER
+
+#check packages outdated
+/usr/lib/update-notifier/apt-check --human-readable
+
+#set file as .token
+file=".token"
+
+#check if exist + permission
+if [[ -f $file && `stat -c %a $file` != 600 ]] ; then
+
+#if true print
+echo 'Warning: .token file has too open permissions'
+
+else
+#permission verified
+echo 'you are good to go'
+
+fi
 
