7b5e104ac37935ea9c68029fff6151c744966d95
hohifrog <hohironyanai@gmail.com>
2022-06-24 18:36:54 +0300

    Revert "ex1_solution (i hope)"
    This reverts commit ab37093c9f5cb4423763f0aa47196dac152237e5.

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -20,3 +20,24 @@ echo 'you are good to go'
 
 fi
 
+#!/bin/bash
+#print welcome msg
+echo Hello $USER
+
+#check packages outdated
+/usr/lib/update-notifier/apt-check --human-readable
+
+#set file as .token
+file=".token"
+
+#check if exist + permission
+if [[ -f $file && `stat -c %a $file` != 600 ]] ; then
+
+#if true print
+echo 'Warning: .token file has too open permissions'
+
+else
+#permission verified
+echo 'you are good to go'
+
+fi
