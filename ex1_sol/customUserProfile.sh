b699337255fe474d8640f316e5e961435596fa92
maor weiss <maorweissb@gmail.com>
2022-06-24 13:10:29 +0300

    ex1 solution # Adding comments and search for the existence of the '.token' file

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,8 +1,8 @@
-echo -e "Hello $(whoami)\n"
+echo -e "Hello $(whoami)\n" # Mission 1
 
-/usr/lib/update-notifier/apt-check --human-readable
+/usr/lib/update-notifier/apt-check --human-readable # Mission2
 
-if [[ "$(stat -c "%a" ~/.token 2>/dev/null)" -ne 600 ]]
+if [[ -f ~/.token && "$(stat -c "%a" ~/.token 2>/dev/null)" -ne 600 ]] # Mission3 - If file exists and permission is not 600
 then
         echo -e  "\nWarning: .token file has too open permissions\n"
         exit 1
