fe7a1c98fb555871554700de0ceae76f04cf534d
EIdoB <eidobennun@gmail.com>
2022-06-26 04:55:51 +0300

    Eido's ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+echo Hello $USER #task 1
+
+/usr/lib/update-notifier/apt-check --human-readable #task 2
+
+FILE=/home/$USER/.token #task 3
+touch $FILE
+STAT=$(stat -c %a $FILE)
+
+
+if [ "$STAT" == "600" ]; then
+    echo "You all set"
+else
+    echo "Warning: .token file has too open permissions"
+fi
+
 
