5fcb7d4d55ff81b26828d476fbf1444441a099c4
eran8888 <eranjunk88@gmail.com>
2022-06-26 18:15:23 +0300

    ex1 solution bash_profile_final

---
+++ ex1_sol/customUserProfile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello $USER"
+echo "$(/usr/lib/update-notifier/apt-check --human-readable)"
+
+touch $HOME/.token
+
+FILE=$HOME/.token
+STATUS=$(stat -c "%a" $FILE)
+
+
+if test -f "$FILE" && [[ $STATUS -ne 600 ]]; then
+   echo "Warning: .token file has too open permissions"
+fi
