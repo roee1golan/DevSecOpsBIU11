0a8949aa62ec2cd7b7ee3d49724ba6c3f9030874
JonathanL <yonilevi0013@gmail.com>
2022-06-26 20:44:05 +0300

    WIP

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+### variables ###
+FILE=/home/$USER/.token
+FILE_PERMISSIONS=stat -c "%a" $FILE
+
+
+printf "Hello $USER\n\n"
+/usr/lib/update-notifier/apt-check --human-readable
+
+if [ -f "$FILE" ]; then
+        if [ $FILE_PERMISSIONS -eq 600 ]
+        then
+                echo "Warning, $FILE file has too open permmissions"
+        fi
+fi
