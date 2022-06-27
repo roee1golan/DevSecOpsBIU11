b69109c5c9d47a15fd4bf695bad95060d17d0544
maor weiss <maorweissb@gmail.com>
2022-06-27 16:59:19 +0300

    ex1 solution # Trying to fix the 'Case 2: .token file with bad permissions' issue #2

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -2,7 +2,7 @@ echo -e "Hello $(whoami)\n" # Mission 1
 
 /usr/lib/update-notifier/apt-check --human-readable # Mission2
 
-if [[ -f ~/.token && "$(stat -c "%a" ~/.token 2>/dev/null)" -ne '600' ]] # Mission3 - If file exists and permission is not 600
+if [[ -f "$HOME/.token" && "$(stat -c "%a" $HOME/.token 2>/dev/null)" -ne '600' ]] # Mission3 - If file exists and permission is not 600
 then
         echo -e  "\nWarning: .token file has too open permissions\n"
         exit 1
