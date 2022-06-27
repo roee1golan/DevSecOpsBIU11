76266bb6e101629eb385b1b6fffe2f1c6435fbb3
JonathanL <yonilevi0013@gmail.com>
2022-06-27 16:03:54 +0300

    ex1 solution - 2 retry

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-printf "Hello $USER\n\n"
+echo Hello $USER
 /usr/lib/update-notifier/apt-check --human-readable
 
-if [ -f "/home/$USER/.token" ]; then
-        FILE_PERM=$(stat -c "%a" /home/$USER/.token)
-        if [ $FILE_PERM == 600 ]
+FILE="$HOME/.token"
+
+if [ -f $FILE ]; then
+        FILE_PERM=$(stat -c "%a" $FILE)
+        if [ "$FILE_PERM" == 600 ]
         then
-                echo "Warning, /home/$USER/.token file has too open permissions"
+                echo Warning, $FILE file has too open permissions
         fi
 fi
