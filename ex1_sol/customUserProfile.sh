04a675d12b77a2c8dbdf9736d6d4abb02a58f88e
maor weiss <maorweissb@gmail.com>
2022-06-27 17:12:52 +0300

    ex1 solution # Trying to fix the 'Case 2: .token file with bad permissions' issue #3

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -5,5 +5,4 @@ echo -e "Hello $(whoami)\n" # Mission 1
 if [[ -f "$HOME/.token" && "$(stat -c "%a" $HOME/.token 2>/dev/null)" -ne '600' ]] # Mission3 - If file exists and permission is not 600
 then
         echo -e  "\nWarning: .token file has too open permissions\n"
-        exit 1
 fi
