0519b56f08b79496f375ddde93bf0675631b6b57
igor_stadnyk <igor.stadnyk@wiliot.com>
2022-06-27 07:51:17 +0300

    ex1 solution change2

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -5,14 +5,13 @@
 echo Hello $USER
 
 /usr/lib/update-notifier/apt-check --human-readable
-
-FILE=/etc/skel/.token
+TOKEN=600
+FILE=~/.token
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else
     echo "$FILE does not exist."
 fi
-TOKEN=600
 xx=$(sudo stat -c '%a' $FILE)
 if [ "$TOKEN" -ne "$xx" ]; then
     echo "Warning: .token file has too open permissions."
