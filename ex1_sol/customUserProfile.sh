5756fbb02f1dcef865e8a603ad7851c9d193d92e
igor_stadnyk <igor.stadnyk@wiliot.com>
2022-06-27 07:09:51 +0300

    ex1 solution change

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,4 +1,5 @@
 
+
 #!/bin/bash
 
 echo Hello $USER
@@ -11,12 +12,8 @@ if [ -f "$FILE" ]; then
 else
     echo "$FILE does not exist."
 fi
-
 TOKEN=600
-xx=$(sudo stat -c '%a' .token)
-
-if [ "$TOKEN" == "$xx" ]; then
-    echo "permissions opened"
-else
+xx=$(sudo stat -c '%a' $FILE)
+if [ "$TOKEN" -ne "$xx" ]; then
     echo "Warning: .token file has too open permissions."
 fi
