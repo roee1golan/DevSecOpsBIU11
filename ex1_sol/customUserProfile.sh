3f825576eefb5b19aa454fe0b4c43198ca5eb916
Yaniv <yaniv.rozenbloom@gmail.com>
2022-06-28 00:32:14 +0300

    Commit final

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,2 +1,10 @@
 
 echo "Hello" $USER
+/usr/lib/update-notifier/apt-check --human-readable
+TOKEN="$HOME/.token"
+if [[ -f $TOKEN && `stat -c %a $TOKEN` -ne 600 ]]
+then
+
+    echo 'Warning: .token file has too open permissions'
+
+fi
