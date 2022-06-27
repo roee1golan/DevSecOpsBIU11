2dc514fea6e932fcdb615da2e0d255fce71311a8
JonathanL <yonilevi0013@gmail.com>
2022-06-27 15:56:56 +0300

    ex1 solution - 1 retry

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -7,6 +7,6 @@ if [ -f "/home/$USER/.token" ]; then
         FILE_PERM=$(stat -c "%a" /home/$USER/.token)
         if [ $FILE_PERM == 600 ]
         then
-                echo "Warning, /home/$USER/.token file has too open permmissions"
+                echo "Warning, /home/$USER/.token file has too open permissions"
         fi
 fi
