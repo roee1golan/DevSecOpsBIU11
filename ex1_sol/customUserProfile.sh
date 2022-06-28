d51cd7e656c5634fc3873c10d399d89112661b57
Alon Faifer <alon.faifer@scopeapp.com>
2022-06-28 11:56:06 +0300

    ex1 solution

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+
+# Prints current user's name
+echo "Hello" $USERNAME
+
+# Extra space
+echo
+
+# Print how many packages are outdated
+/usr/lib/update-notifier/apt-check --human-readable
+
+# Extra space
+echo
+
+# Checking if file named ".token" exists in the user's home directoy
+
+# Creates a variable called FILE with .token and CORRECTPERM for read and write by the user only permission
+FILE=.token
+CORRECTPERM=600
+if test -f /home/$USERNAME/"$FILE"
+then
+ FILEPERM=$(stat -c "%a" /home/$USERNAME/"$FILE")
+ if [ $FILEPERM != $CORRECTPERM ]
+ then
+  echo "Warning: .token file has too open permissions"
+ fi
+fi
 
