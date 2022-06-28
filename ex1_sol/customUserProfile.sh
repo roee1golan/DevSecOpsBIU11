bdb79ccb53adc4321d03c368a8542c12394125cb
simon <simonshmo@gmail.com>
2022-06-28 16:32:58 +0300

    ex1_test1

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,7 +1,14 @@
+!#/bin/bash
+
 echo "****Hello $USER****"
 echo `/usr/lib/update-notifier/apt-check --human-readable`
 FILE=$HOME/.token
+PER="stat -c "%a"
+if [ -f $FILE ]; then
+  echo "**File Exist**"
+else
+  echo "**File Not Exist**"
 
-if [ -f $FILE && "stat -c "%a" $FILE" -ne 600  ] ; then
+if [ $PER $FILE" -ne 600 ] ; then
   echo "**Warning: .token file has too open permissions**"
 fi
